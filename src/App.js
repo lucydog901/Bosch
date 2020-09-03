7ad367bf813200e8d99e5c4edc527665d4b65118
Thomasin Durgin <td@thomasin.com>
2020-09-03 12:38:45 -0500

    added image grid in css

--- src/App.js
+++ src/App.js
@@ -18,15 +18,15 @@ class App extends Component {
 
     return (
        <div className="container">
-        <div className="col-xs-12">
-        <img src={require('./images/header6.png')} alt="header" width="750"/>
+        <div className="image-grid">
+        
         {this.state.paintings.map((painting) => (
           <div className="card">
             <div className="card-body">
               <h5 className="card-title">{painting.title}</h5>
               <h6 className="card-subtitle mb-2 text-muted">{painting.creator}</h6>
               <h6 className="card-subtitle mb-2 text-muted">source: {painting.source}</h6>
-              <img src={painting.url} alt="Artwork" width="500" />
+              <img src={painting.url} alt="Artwork" width="250" />
             </div>
           </div>
         ))}

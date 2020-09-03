54be8f2548194a7c93ebcb0d2f68ecb3146bdc00
Thomasin Durgin <td@thomasin.com>
2020-09-03 11:40:08 -0500

    added header and changed background color

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './App.css'
 
 class App extends Component {
   state = {
@@ -18,7 +19,7 @@ class App extends Component {
     return (
        <div className="container">
         <div className="col-xs-12">
-        <h1>Bosch Bash</h1>
+        <img src={require('./images/header6.png')} alt="header" width="750"/>
         {this.state.paintings.map((painting) => (
           <div className="card">
             <div className="card-body">

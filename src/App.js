a9d23fe5fd7f61ff38d3a11b3c406f77407d17eb
Thomasin Durgin <td@thomasin.com>
2020-09-04 16:04:51 -0500

    created and added header component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,43 @@
 import React, { Component } from 'react';
-import './App.css'
+import './App.css';
+import Header from './components/Header';
+
+
 
 class App extends Component {
   state = {
     paintings: []
   }
+
   componentDidMount() {
     fetch('https://api.creativecommons.engineering/v1/images?format=json&source=met,clevelandmuseum,smithsonian_portrait_gallery,smithsonian_cooper_hewitt_museum&creator=Gauguin ')
-    .then(res => res.json())
-    .then((data) => {
-      this.setState({ paintings: data.results})
-      console.log(this.state.paintings)
-    })
-    .catch(console.log)
+      .then(res => res.json())
+      .then((data) => {
+        this.setState({ paintings: data.results })
+        console.log(this.state.paintings)
+      })
+      .catch(console.log)
   }
   render() {
 
     return (
-       <div className="container">
-        <div className="image-grid">
-        
-        {this.state.paintings.map((painting) => (
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">{painting.title}</h5>
-              <h6 className="card-subtitle mb-2 text-muted">{painting.creator}</h6>
-              <h6 className="card-subtitle mb-2 text-muted">source: {painting.source}</h6>
-              <img src={painting.url} alt="Artwork" width="250" />
-            </div>
+      <div>
+        <Header />
+        <div className="container">
+          <div className="image-grid">
+            {this.state.paintings.map((painting) => (
+              <div className="card">
+                <div className="card-body px-2">
+                  <h5 className="card-title">{painting.title}</h5>
+                  <h6 className="card-subtitle mb-2 text-muted">{painting.creator}</h6>
+                  <h6 className="card-subtitle mb-2 text-muted">source: {painting.source}</h6>
+                  <img src={painting.url} alt="Artwork" width="250" />
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
         </div>
-       </div>
+      </div>
     );
   }
 }
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import logoUrl from '../images/header6.png'
+
+
+const Header = () => (
+  <div className="header">
+    <img src={logoUrl} alt="Bosch Bash logo" width = '750' />
+  </div>
+)
+export default Header;

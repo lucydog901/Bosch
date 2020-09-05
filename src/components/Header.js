5f927267311f660f031f69dd2755e7a4f7070138
Thomasin Durgin <td@thomasin.com>
2020-09-05 18:37:07 -0500

    resized logo and header text

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,17 +6,17 @@ const Header = () => (
   <div className="header">
     <nav class="navbar navbar-expand-lg navbar-dark navbar-custom align-items-end">
       <a class="navbar-brand" href="https://www.boschbash.com/">
-        <img src={logoUrl} alt="Bosch Bash logo" class="img-fluid" width="750" />
+        <img src={logoUrl} alt="Bosch Bash logo" class="img-fluid" width="650" />
       </a>
       <ul class="navbar-nav ml-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Search Museums<span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#"><h5>Search Museums</h5><span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">View Gallery</a>
+        <a class="nav-link" href="#"><h5>View Gallery</h5></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">About H. Bosch</a>
+        <a class="nav-link" href="#"><h5>About H. Bosch</h5></a>
       </li>
     </ul>
 

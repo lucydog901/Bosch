4bd7b8ee9786cfc9c99443a72fe5fb939bf0c352
Thomasin Durgin <td@thomasin.com>
2020-09-06 18:23:16 -0500

    troublshooting netflify build error

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,6 +5,7 @@ import still from '../images/still_life.png';
 
 
 const Welcome = () => (
+  <div className="welcome">
   <div class="container p-3 my-4">
     <div class="d-flex flex-row">
     <div class="p-4"><img src={lisaUrl} alt="Mona Lisa" class="img-fluid" class="rounded" width="350"/>
@@ -13,6 +14,7 @@ const Welcome = () => (
     <p>Create your own masterpiece with figures from Hieronymus Bosch's painting "The Garden of Earthly Delights". Boschicize artworks from The Metropolitan Museum of Art and The Cleveland Museum of Art and download your creations. You may search the museums for an image, or choose one from the gallery.</p>
     <img src={still} alt="Still Life" class="img-fluid" class="rounded" width="500"/>
     </div>
+  </div>
   </div>
   </div>
 

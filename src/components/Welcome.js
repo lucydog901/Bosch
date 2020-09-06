c6aeabb6ab4d7581f4cddd893ab7e8cd2df8f931
Thomasin Durgin <td@thomasin.com>
2020-09-06 17:17:44 -0500

    added examples to home page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -7,11 +7,11 @@ import stillUrl from '../images/still_life.png';
 const Welcome = () => (
   <div class="container p-3 my-4">
     <div class="d-flex flex-row">
-    <div class="p-2"><img src={lisaUrl} alt="Mona Lisa" class="img-fluid" class="rounded" width="300"/>
+    <div class="p-4"><img src={lisaUrl} alt="Mona Lisa" class="img-fluid" class="rounded" width="350"/>
     </div>
-    <div class="p-2">
+    <div class="p-4">
     <p>Create your own masterpiece with figures from Hieronymus Bosch's painting "The Garden of Earthly Delights". Boschicize artworks from The Metropolitan Museum of Art and The Cleveland Museum of Art and download your creations. You may search the museums for an image, or choose one from the gallery.</p>
-    <img src={stillUrl} alt="Still Life" class="img-fluid" class="rounded" width="300"/>
+    <img src={stillUrl} alt="Still Life" class="img-fluid" class="rounded" width="500"/>
     </div>
   </div>
   </div>

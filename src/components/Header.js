5aa1dddbc7787705eba72cfd11ef3a9707be4469
Thomasin Durgin <td@thomasin.com>
2020-09-07 10:45:30 -0500

    troubleshooting Netlify error

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logoUrl from '../images/header_trans.png';
+import logoUrl from '../images/header_trans4.png';
 
 
 const Header = () => (
   <div className="header">
     <nav class="navbar navbar-expand-lg navbar-dark navbar-custom align-items-end">
       <a class="navbar-brand" href="https://www.boschbash.com/">
-        <img src={logoUrl} alt="Bosch Bash logo" class="img-fluid" width="650" />
+        <img src={logoUrl} alt="Bosch Bash logo" class="img-fluid" width="600" />
       </a>
       <ul class="navbar-nav ml-auto">
       <li class="nav-item active">
@@ -14,6 +14,9 @@ const Header = () => (
       </li>
       <li class="nav-item">
         <a class="nav-link" href="https://www.boschbash.com/"><h5>Search</h5></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="https://www.boschbash.com/"><h5>Gallery</h5></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="https://www.boschbash.com/"><h5>Upload</h5></a>
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -7,15 +7,20 @@ import still from '../images/still_life.png';
 const Welcome = () => (
   <div class="container p-3 my-4">
     <div class="d-flex flex-row">
-    <div class="p-4"><img src={lisaUrl} alt="Mona Lisa" class="img-fluid" class="rounded" width="350"/>
+      <div class="col">
+          <div class="p-4"><img src={lisaUrl} alt="Mona Lisa" class="rounded" width="350" class="img-fluid" />
+          </div>
+      </div>
+      <div class="col">
+        <div class="p-4">
+            <p>Create your own masterpiece with figures from Hieronymus Bosch's painting <i>The Garden of Earthly Delights</i>. Boschicize famous artworks from the public domain or use your own photos. You may search art museum collections for an image, choose one from the gallery, or upload your own.</p><br></br>
+            <img src={still} alt="Still Life" class="rounded" width="500" class="img-fluid" />
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="p-4">
-    <p>Create your own masterpiece with figures from Hieronymus Bosch's painting "The Garden of Earthly Delights". Boschicize famous artworks in the public domain or use your own photos. You may search the museums for an image, choose one from the gallery, or upload your own.</p><br></br><br></br><br></br>
-    <img src={still} alt="Still Life" class="img-fluid" class="rounded" width="500"/>
-    </div>
-  </div>
-  </div>
-  
+
+
 
 )
 export default Welcome;
